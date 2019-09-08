b23259777e06d0e4506515dfbcbb63578f8c3286
BBOYYUE <bboyxiaoyue@outlook.com>
2019-09-08 17:13:33 +0800

    a

--- app/Project/Blog/blogCore.php
+++ app/Project/Blog/blogCore.php
@@ -4,17 +4,18 @@ namespace App\Project\Blog;
 use Illuminate\Support\Facades\Storage;
 
 class blogCore{
-     public  $htmlHead = '
-	<!DOCTYPE html>
-	<html>
-	<head>
+    public  $htmlHead = '
+	    <!DOCTYPE html>
+	    <html>
+	    <head>
     		<meta charset="utf-8">
     		<meta name="viewport" content="width=device-width, initial-scale=1">
-   		 <link rel="dns-prefetch" href="//fonts.gstatic.com">
-   		 <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-  		 <link href="https://cmfac.com/editormd/css/editormd.css" rel="stylesheet">
-	</head><body class="markdown-body editormd-preview-container">';
-public $htmlBody = '</body></html>';
+   		    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+   		    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  		    <link href="https://cmfac.com/editormd/css/editormd.css" rel="stylesheet">
+        </head>
+        <body class="markdown-body editormd-preview-container">';
+    public $htmlBody = '</body></html>';
 
    
     function __construct(){
@@ -47,41 +48,63 @@ public $htmlBody = '</body></html>';
         $format->code = 1;
     }
     function createDir($request,$format){
-        $makedir = $this->dir->create(
-            [
-                'name'=>$request->input('name'),
-                'url'=>time(),
-                'summary'=>$request->input('summary'),
-                'created_at'=>time()
-            ]
-        );
-        $userdir = $this->userdir->create(
-            [
-                'userid'=>\Auth::id(),
-                'dirid'=>$makedir->id
-            ]
-        );
-        $format->code = 1;
-        $format->data = ['message'=>'保存成功','url'=>$makedir->url];
+        try{
+            $makedir = $this->dir->create(
+                [
+                    'name'=>$request->input('name'),
+                    'url'=>time(),
+                    'summary'=>$request->input('summary'),
+                    'created_at'=>time()
+                ]
+            );
+            $userdir = $this->userdir->create(
+                [
+                    'userid'=>\Auth::id(),
+                    'dirid'=>$makedir->id
+                ]
+            );
+           $save = ($makedir&&$userdir);
+    
+           if($save){
+               $format->code = 1;
+               $format->data = ['message'=>'保存成功','url'=>$makedir->url];
+           }else{
+                throw new \Exception("保存失败");
+           }
+
+        }catch(\Exception $e){
+            $format->code = 4;
+            $format->data = $e->getMessage();
+        }
     }
     function createFile($request,$format){
-        $dirid = $request->input('dirid');
-        $dir = $request->input('dir');
-        $file = time();
-        $html = $file.'.html';
-        $filename = $request->input('title');
-        $makefile = $this->makefile->makefile($dir,$file,$filename,'# hello word');
-        $makehtml = $this->makefile->makefile($dir,$html,$filename,'<h1>Hello word</h1>');
-        $dirfile = $this->dirfile->create(
-            [
-                'fileid'=>$makefile['id'],
-                'htmlid'=>$makehtml['id'],
-                'dirid'=>$dirid,
-                'created_at'=>time()
-            ]
-        );
-        $format->code = 1;
-        $format->data = ['message'=>'保存成功','url'=>$makefile['url']];
+        try{
+           $dirid = $request->input('dirid');
+           $dir = $request->input('dir');
+           $file = time();
+           $html = $file.'.html';
+           $filename = $request->input('title');
+           $makefile = $this->makefile->makefile($dir,$file,$filename,'# hello word');
+           $makehtml = $this->makefile->makefile($dir,$html,$filename,'<h1>Hello word</h1>');
+           $dirfile = $this->dirfile->create(
+                [
+                    'fileid'=>$makefile['id'],
+                    'htmlid'=>$makehtml['id'],
+                    'dirid'=>$dirid,
+                    'created_at'=>time()
+                ]
+            );
+            if($dirfile){
+                $format->code = 1;
+                $format->data = ['message'=>'保存成功','url'=>$makefile['url']];
+            }else{
+                throw new \Exception("保存失败");
+            }
+
+        }catch(\Exception $e){
+            $format->code = 4;
+            $format->data = $e->getMessage();
+        }
     }
     function showMd($request,$format){
         if(empty($request->input('path'))) $doc = "#朝花夕拾\n繁花在时光中凋零，在记忆中盛开。";
@@ -97,14 +120,17 @@ public $htmlBody = '</body></html>';
     }
 
     function updateFile($request,$format){
-        Storage::put($request->input('path'), $request->input('doc'));
-	Storage::put($request->input('path').'.html', $this->htmlHead.$request->input('test-editor-html-code').$this->htmlBody);
-	$format->code = 1;
-        $format->data = ['message'=>'保存成功'];
-    }
-
-
-
-
-    
+        try{
+            $savedoc = Storage::put($request->input('path'), $request->input('doc'));
+            $savefile = Storage::put($request->input('path').'.html', $this->htmlHead.$request->input('test-editor-html-code').$this->htmlBody);
+            $save = ($savedoc&&$savefile);
+            if($save){
+	            $format->code = 1;
+                $format->data = ['message'=>'保存成功'];
+            }
+        }catch(\Exception $e){
+            $format->code = 4;
+            $format->data = $e->getMessage();            
+        }
+    }    
 }
