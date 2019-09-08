c2595811834765ba008f7d6531eb738b6e71794f
BBOYYUE <bboyxiaoyue@outlook.com>
2019-09-08 23:54:50 +0800

    new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,15 @@
 @endsection
 @section('content')
 <div class="container-fluid ">
-    <div class="jumbotron jumbotron-fluid">
-  <div class="container text-center">
-    <h1 class="display-3">Welcome</h1>
-    <p class="lead">To Cmfac.com</p>
-    <hr class="my-4">
-    <p>This website is about learning. It mainly covers computers and networks. It also has some mathematics and algorithms. This site is not authoritative, so the content may be incorrect. But it's open source, and everyone can edit and modify it.</p>
-  </div>
-</div> 
+  <div class="row" style="padding:0">
+    <div class="jumbotron jumbotron-fluid col-12">
+      <div class="container text-center" > 
+        <h1 class="display-3">Welcome</h1>
+        <p class="lead">To Cmfac.com</p>
+        <hr class="my-4">
+        <p>This website is about learning. It mainly covers computers and networks. It also has some mathematics and algorithms. This site is not authoritative, so the content may be incorrect. But it's open source, and everyone can edit and modify it.</p>
+      </div>
+    </div> 
+</div>
 </div>
 @endsection
