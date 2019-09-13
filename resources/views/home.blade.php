d92251dce81c2252f3bfa4403ae802c248c6b9b0
BBOYYUE <bboyxiaoyue@outlook.com>
2019-09-13 17:47:02 +0800

    new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,47 +23,145 @@
 </div>
     <script src="/js/jquery.js"></script>
 <script>
-  $(window).ready(function(){
-  var audio = document.querySelector("audio");
-  audio.src = "http://cmfac.com/storage/music.mp3";
-  //audio.play();
-  html = "<div style='width:15px;background:#fff;display:inline-block;margin:1px;position:relative;bottom:0;'></div>";
-  for(i=0;i<$(window).width()/40;i++){ 
-    $('.top').append(html);
+  if($(window).width()>728){
+    boxwidth = "1rem";
+  }else{
+    boxwidth = '.3rem';
   }
-  console.log($(window).width()/40);
-
-  load =audio.addEventListener('canplaythrough',function(){
-    audio.controls =true;
-   start =audio.addEventListener('play',function(){
-    $('.top').show();
-   a = (function(){
-       for(i=1;i<$(window).width()/200;i++){
-       $('.top').children('div').eq(i).animate({height:Math.floor(Math.random()*2)+'rem'},200);
-     }
-       for($(window).width()/200;i<$(window).width()/100;i++){
-       $('.top').children('div').eq(i).animate({height:Math.floor(Math.random()*3)+'rem'},200);
-     }
-       for($(window).width()/100;i<$(window).width()*3/200;i++){
-       $('.top').children('div').eq(i).animate({height:Math.floor(Math.random()*4)+'rem'},200);
-     }
-       for($(window).width()*3/200;i<$(window).width()/50;i++){
-       $('.top').children('div').eq(i).animate({height:Math.floor(Math.random()*3)+'rem'},200);
-     }
-       for($(window).width()/50;i<$(window).width()/40;i++){
-       $('.top').children('div').eq(i).animate({height:Math.floor(Math.random()*2)+'rem'},200);
-     }
-   window.requestAnimationFrame(a);
-   })
-   c = window.requestAnimationFrame(a);
-   });
-  });
-   end = audio.addEventListener('pause',function(){
-    $('.top').hide();
-   })
-   load = audio.addEventListener('waiting',function(){
-    $('.top').hide();
-   })
-  });
+  html = "<div style='width:"+boxwidth+";background:#fff;display:inline-block;margin:1px;position:relative;bottom:0;'></div>";
+  for(i=0;i<32;i++){ 
+    $('.top').prepend(html);
+  }
 </script>
+<script>
+    (window)
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    var analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 32;
+    var bufferLength = analyser.fftSize;
+    var dataArray = new Uint8Array(bufferLength);
+    analyser.getByteTimeDomainData(dataArray);
+
+
+  
+
+    // 获取音频 
+
+    function getData() {
+        source = audioCtx.createBufferSource();
+        var request = new XMLHttpRequest();
+
+        request.open('GET', '/storage/music.mp3', true);
+
+        request.responseType = 'arraybuffer';
+
+
+        request.onload = function () {
+            var audioData = request.response;
+
+            audioCtx.decodeAudioData(audioData, function (buffer) {
+                source.buffer = buffer;
+
+                source.connect(audioCtx.destination);
+                source.loop = true;
+            },
+
+                function (e) { "Error with decoding audio data" + e.err });
+        }
+
+        request.send()
+    } 
+    getData();
+    source.connect(analyser);
+
+
+       // draw an oscilloscope of the current audio source
+    function draw() {
+
+        drawVisual = requestAnimationFrame(draw);
+        analyser.getByteTimeDomainData(dataArray);
+        for(i=0;i<bufferLength;i++){
+          $('.top').children('div').eq(i).animate({height:((dataArray[i]/128.0)-.3)*3+'rem'},1000/60);
+        }
+    }
+      draw();
+      source.start(0);
+      $('.top').click(function(){
+        if(audioCtx.state === 'running') {
+          audioCtx.suspend().then(function() {
+            window.cancelAnimationFrame(drawVisual);
+          });
+        } else if(audioCtx.state === 'suspended') {
+          audioCtx.resume().then(function() {
+            draw();
+          });
+        }
+      })
+      
+    
+  
+</script>
+        <script src="/js/jquery.js"></script>
+    <script>
+if($(window).width()>767){
+$('.card').mouseenter(function(){
+    $(this).children('.card-footer').fadeIn();
+    $(this).css('z-index','+=1');
+    $(this).children('.card-footer').css('z-index','+=2');
+    $(this).animate({margin:"-=.5rem"});
+})
+$('.card').mouseleave(function(){
+    $(this).children('.card-footer').fadeOut();
+    $(this).children('.card-footer').css('z-index','');
+    $(this).css('z-index','');
+    $(this).animate({margin:""});
+})
+}else{
+    $('.card').click(function(){
+        $(this).children('.card-footer').toggle(200);
+    $(this).children('.card-footer').css('z-index','+=2');
+    })
+}
+    function getPost(formData,url,callback,reload=true){
+        var request = new XMLHttpRequest();
+        request.open('post',url);
+        request.send(formData);
+        
+        request.onload = e => {
+            $('.alert-info').text('正在保存');
+            $('.alert-info').fadeIn(1000);
+        };
+        request.onloadend = e => {
+            data = JSON.parse(request.responseText);
+            console.log(data);
+            console.log(data.code);
+            if(data.code==1){
+                $('.alert-success').text(data.data.message);
+                if($('.alert-info').fadeOut()){
+                    $('.alert-success').fadeIn(3000);
+                    $('.alert-success').fadeOut(1000);
+                    if(reload){setTimeout(function(){location.reload()},5000)};
+                }
+            }else if(data.code==4){
+                $('.alert-info').fadeOut(1000);
+                $('.alert-danger').text(data.data.message);
+                $('.alert-danger').fadeIn(3000);
+            }else{
+                $('.alert-info').fadeOut(1000);
+                $('.alert-danger').text(data.data.message);
+                $('.alert-danger').fadeIn(3000);
+            } 
+        };
+        request.ontimeout = e =>{
+            $('.alert-info').fadeOut(1000);
+            $('.alert-secondary').text('请求超时');
+            $('.alert-secondary').fadeIn(3000);
+        };
+        request.onerror = e =>{
+            $('.alert-info').fadeOut(1000);
+            $('.alert-waring').text('请求失败');
+            $('.alert-waring').fadeIn(3000);
+        };
+    }
+    </script>
 @endsection
